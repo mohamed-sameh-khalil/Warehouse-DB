78dbaaf7d35245089f119bd134716948b784a809
Nour E-Din Osama Mohamed <41454500+nourmat@users.noreply.github.com>
2019-05-08 19:35:22 +0200

    Add files via upload

---
+++ insertion.sql
@@ -0,0 +1,40 @@
+INSERT INTO Entity
+Values (1, "Westroos Company",+20111, "amidn@westros.com",+20112,"Egypt","Cairo","Mokattam","nine","8127");
+
+INSERT INTO Supplier 
+Values (1,'2019-5-8','2019-12-30');
+
+
+
+INSERT INTO Entity
+Values (2, "winterfell Company",+20111, "amidn@winterfell.com",+20223,"Spain","barcelona","NAN","81","54");
+
+INSERT INTO Distributor
+Values (2,"99987887");
+
+INSERT INTO Company
+values (2,"555555");
+
+
+
+INSERT INTO Category 
+values (1,"Samsung Mobile Phone",400,"Samsung",5,3,4,"Mobile Phone comes with charger and headphone");
+
+INSERT INTO Category 
+values (2,"LG TV",2500,"LG",20,10,5,"Smart TV comes with remote and two AAA batteries");
+
+
+
+INSERT INTO Mobiles 
+values (1,5,"Note 4");
+
+INSERT INTO TV
+values (2,43,"Smart ooo","LED");
+
+
+
+INSERT INTO item
+values (1,'2019-1-10 01:56:11',"Blue","Pending",'A','33',1,NULL,1);
+
+INSERT INTO item
+values (2,'2019-5-8 12:24:01',"Black","Orderd",'S','21',1,2,2);
--- sql.sql
+++ sql.sql
@@ -1,144 +1,146 @@
-CREATE TABLE `Entity`
+CREATE TABLE Entity
 (
-    `ID` INTEGER PRIMARY KEY NOT NULL,
-    `name` VARCHAR(100) NOT NULL,
-    `phone` VARCHAR(20) NOT NULL,
-    `email` VARCHAR(25) NOT NULL,
-    `fax` VARCHAR(30),
-    `country` VARCHAR(20) NOT NULL,
-    `state` VARCHAR(20) NOT NULL,
-    `city` VARCHAR(20) NOT NULL,
-    `street` VARCHAR(20) NOT NULL,
-    `building` VARCHAR(20) NOT NULL
+    ID INTEGER PRIMARY KEY NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    email VARCHAR(25) NOT NULL,
+    fax VARCHAR(30),
+    country VARCHAR(20) NOT NULL,
+    state VARCHAR(20) NOT NULL,
+    city VARCHAR(20) NOT NULL,
+    street VARCHAR(20) NOT NULL,
+    building VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE `Supplier`
+CREATE TABLE Supplier
 (
-    `ID` INTEGER PRIMARY KEY NOT NULL,
-    `contract_start` DATE NOT NULL,
-    `contract_end` DATE,
-    FOREIGN KEY (`ID`) REFERENCES `Entity` (`ID`)
+    ID INTEGER PRIMARY KEY NOT NULL,
+    contract_start DATE NOT NULL,
+    contract_end DATE,
+    FOREIGN KEY (ID) REFERENCES Entity (ID)
     ON DELETE RESTRICT
     ON UPDATE CASCADE
 );
 
-CREATE TABLE `Items`
+CREATE TABLE Distributor
 (
-    `ID` INTEGER PRIMARY KEY NOT NULL,
-    `arrival_time` TIMESTAMP NOT NULL,
-    `color_pallete` VARCHAR(10) NOT NULL,
+    ID INTEGER PRIMARY KEY NOT NULL,
+    tax_No VARCHAR(40) NOT NULL,
+    FOREIGN KEY (ID) REFERENCES Entity (ID)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE Company
+(
+    ID INTEGER PRIMARY KEY NOT NULL,
+    register_No VARCHAR(40) NOT NULL,
+    FOREIGN KEY (ID) REFERENCES Distributor (ID)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE Person
+(
+    ID INTEGER PRIMARY KEY NOT NULL,
+    SSN VARCHAR(20) NOT NULL,
+    FOREIGN KEY (ID) REFERENCES Distributor (ID)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE Items
+(
+    ID INTEGER PRIMARY KEY NOT NULL,
+    arrival_time TIMESTAMP NOT NULL,
+    color_pallete VARCHAR(10) NOT NULL,
     `status` varchar(255) NOT NULL,
-    `location_letter` CHAR(1) NOT NULL,
-    `location_num` CHAR(3) NOT NULL,
-    `supplier_ID` INTEGER NOT NULL,
-    `distributor_ID` INTEGER NOT NULL,
-    `Category_ID` INTEGER NOT NULL,
-    FOREIGN KEY (`supplier_ID`) REFERENCES `Supplier` (`ID`)
+    location_letter CHAR(1) NOT NULL,
+    location_num CHAR(3) NOT NULL,
+    supplier_ID INTEGER NOT NULL,
+    distributor_ID INTEGER,
+    Category_ID INTEGER NOT NULL,
+    FOREIGN KEY (supplier_ID) REFERENCES Supplier (ID)
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
-    FOREIGN KEY (`distributor_ID`) REFERENCES `Distributor`(`ID`)
+    FOREIGN KEY (distributor_ID) REFERENCES Distributor(ID)
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
-    FOREIGN KEY (`Category_ID`) REFERENCES `Category` (`ID`)
+    FOREIGN KEY (Category_ID) REFERENCES Category (ID)
     ON DELETE RESTRICT
     ON UPDATE CASCADE
 );
 
-CREATE TABLE `Distributor`
+CREATE TABLE Category
 (
-    `ID` INTEGER PRIMARY KEY NOT NULL,
-    `tax_No` VARCHAR(40) NOT NULL,
-    FOREIGN KEY (`ID`) REFERENCES `Entity` (`ID`)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE
-);
-
-CREATE TABLE `Company`
-(
-    `ID` INTEGER PRIMARY KEY NOT NULL,
-    `register_No` VARCHAR(40) NOT NULL,
-    FOREIGN KEY (`ID`) REFERENCES `Distributor` (`ID`)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE
-);
-
-CREATE TABLE `Person`
-(
-    `ID` INTEGER PRIMARY KEY NOT NULL,
-    `SSN` VARCHAR(20) NOT NULL,
-    FOREIGN KEY (`ID`) REFERENCES `Distributor` (`ID`)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE
-);
-
-CREATE TABLE `TV`
-(
-    `ID` INTEGER PRIMARY KEY NOT NULL,
-    `screensize` FLOAT NOT NULL,
-    `Model_No` VARCHAR(20),
-    `screentype` VARCHAR(10),
-    FOREIGN KEY (`ID`) REFERENCES `Category` (`ID`)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE
-);
-
-CREATE TABLE `Chargers`
-(
-    `ID` INTEGER PRIMARY KEY NOT NULL,
-    `voltage` FLOAT NOT NULL,
-    `charger_Type` VARCHAR(40) NOT NULL,
-    FOREIGN KEY (`ID`) REFERENCES `Category` (`ID`)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE
-);
-
-CREATE TABLE `Category`
-(
-    `ID` INTEGER PRIMARY KEY NOT NULL,
-    `name` VARCHAR(100) NOT NULL,
-    `weight` FLOAT NOT NULL,
-    `company` VARCHAR(50) NOT NULL,
-    `length` FLOAT NOT NULL,
-    `width` FLOAT NOT NULL,
-    `height` FLOAT NOT NULL,
+    ID INTEGER PRIMARY KEY NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    weight FLOAT NOT NULL,
+    company VARCHAR(50) NOT NULL,
+    length FLOAT NOT NULL,
+    width FLOAT NOT NULL,
+    height FLOAT NOT NULL,
     `desc` VARCHAR(500)
 );
 
-CREATE TABLE `Mobiles`
+CREATE TABLE TV
 (
-    `ID` INTEGER PRIMARY KEY NOT NULL,
-    `screensize` FLOAT NOT NULL,
-    `Model_No` VARCHAR(20) NOT NULL,
-    FOREIGN KEY (`ID`) REFERENCES `Category` (`ID`)
+    ID INTEGER PRIMARY KEY NOT NULL,
+    screensize FLOAT NOT NULL,
+    Model_No VARCHAR(20),
+    screentype VARCHAR(10),
+    FOREIGN KEY (ID) REFERENCES Category (ID)
     ON DELETE RESTRICT
     ON UPDATE CASCADE
 );
 
-CREATE TABLE `Video_Games`
+CREATE TABLE Chargers
 (
-    `ID` INTEGER PRIMARY KEY NOT NULL,
-    `genre` VARCHAR(20),
-    `release_date` DATE,
-    FOREIGN KEY (`ID`) REFERENCES `Category` (`ID`)
+    ID INTEGER PRIMARY KEY NOT NULL,
+    voltage FLOAT NOT NULL,
+    charger_Type VARCHAR(40) NOT NULL,
+    FOREIGN KEY (ID) REFERENCES Category (ID)
     ON DELETE RESTRICT
     ON UPDATE CASCADE
 );
 
-CREATE TABLE `platform`
+CREATE TABLE Mobiles
 (
-    `name` VARCHAR(20) NOT NULL,
-    `version` VARCHAR(20) NOT NULL,
-    `video_game_id` INTEGER PRIMARY KEY NOT NULL,
-    FOREIGN KEY (`video_game_id`) REFERENCES `Video_Games` (`ID`)
+    ID INTEGER PRIMARY KEY NOT NULL,
+    screensize FLOAT NOT NULL,
+    Model_No VARCHAR(20) NOT NULL,
+    FOREIGN KEY (ID) REFERENCES Category (ID)
     ON DELETE RESTRICT
     ON UPDATE CASCADE
 );
 
-CREATE TABLE `Color`
+CREATE TABLE Video_Games
 (
-    `color_pallete` VARCHAR(10) NOT NULL,
-    `Category_ID` INTEGER PRIMARY KEY NOT NULL,
-    FOREIGN KEY (`Category_ID`) REFERENCES `Category` (`ID`)
+    ID INTEGER PRIMARY KEY NOT NULL,
+    genre VARCHAR(20),
+    release_date DATE,
+    FOREIGN KEY (ID) REFERENCES Category (ID)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE platform
+(
+    name VARCHAR(20) NOT NULL,
+    version VARCHAR(20) NOT NULL,
+    video_game_id INTEGER NOT NULL,
+    PRIMARY KEY (name,version,video_game_id),
+    FOREIGN KEY (video_game_id) REFERENCES Video_Games (ID)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE Color
+(
+    color_pallete VARCHAR(10) NOT NULL,
+    Category_ID INTEGER NOT NULL,
+    PRIMARY KEY (color_pallete, category_ID),    
+    FOREIGN KEY (Category_ID) REFERENCES Category (ID)
     ON DELETE RESTRICT
     ON UPDATE CASCADE
 );
