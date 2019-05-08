bb9e5a68397b1064f438d01b7021b0c225c48ab5
Nour E-Din Osama Mohamed <41454500+nourmat@users.noreply.github.com>
2019-05-08 04:35:11 +0200

    Add files via upload

--- sql.sql
+++ sql.sql
@@ -1,130 +1,144 @@
 CREATE TABLE `Entity`
 (
-  `ID` INTEGER PRIMARY KEY,
-  `name` VARCHAR(100),
-  `phone` VARCHAR(20),
-  `email` VARCHAR(25),
-  `fax` VARCHAR(30),
-  `country` VARCHAR(20),
-  `state` VARCHAR(20),
-  `city` VARCHAR(20),
-  `street` VARCHAR(20),
-  `building` VARCHAR(20)
+    `ID` INTEGER PRIMARY KEY NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `phone` VARCHAR(20) NOT NULL,
+    `email` VARCHAR(25) NOT NULL,
+    `fax` VARCHAR(30),
+    `country` VARCHAR(20) NOT NULL,
+    `state` VARCHAR(20) NOT NULL,
+    `city` VARCHAR(20) NOT NULL,
+    `street` VARCHAR(20) NOT NULL,
+    `building` VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE `Supplier`
 (
-  `ID` INTEGER PRIMARY KEY,
-  `contract_start` DATE,
-  `contract_end` DATE
+    `ID` INTEGER PRIMARY KEY NOT NULL,
+    `contract_start` DATE NOT NULL,
+    `contract_end` DATE,
+    FOREIGN KEY (`ID`) REFERENCES `Entity` (`ID`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE `Items`
 (
-  `ID` INTEGER PRIMARY KEY,
-  `arrival_time` TIMESTAMP,
-  `color_pallete` VARCHAR(10),
-  `status` varchar(255),
-  `location_letter` CHAR(1),
-  `location_num` CHAR(3),
-  `supplier_ID` INTEGER,
-  `distributor_ID` INTEGER,
-  `Category_ID` INTEGER
+    `ID` INTEGER PRIMARY KEY NOT NULL,
+    `arrival_time` TIMESTAMP NOT NULL,
+    `color_pallete` VARCHAR(10) NOT NULL,
+    `status` varchar(255) NOT NULL,
+    `location_letter` CHAR(1) NOT NULL,
+    `location_num` CHAR(3) NOT NULL,
+    `supplier_ID` INTEGER NOT NULL,
+    `distributor_ID` INTEGER NOT NULL,
+    `Category_ID` INTEGER NOT NULL,
+    FOREIGN KEY (`supplier_ID`) REFERENCES `Supplier` (`ID`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+    FOREIGN KEY (`distributor_ID`) REFERENCES `Distributor`(`ID`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+    FOREIGN KEY (`Category_ID`) REFERENCES `Category` (`ID`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE `Distributor`
 (
-  `ID` INTEGER PRIMARY KEY,
-  `tax_No` VARCHAR(40)
+    `ID` INTEGER PRIMARY KEY NOT NULL,
+    `tax_No` VARCHAR(40) NOT NULL,
+    FOREIGN KEY (`ID`) REFERENCES `Entity` (`ID`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE `Company`
 (
-  `ID` INTEGER PRIMARY KEY,
-  `register_No` VARCHAR(40)
+    `ID` INTEGER PRIMARY KEY NOT NULL,
+    `register_No` VARCHAR(40) NOT NULL,
+    FOREIGN KEY (`ID`) REFERENCES `Distributor` (`ID`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE `Person`
 (
-  `ID` INTEGER PRIMARY KEY,
-  `SSN` VARCHAR(20)
+    `ID` INTEGER PRIMARY KEY NOT NULL,
+    `SSN` VARCHAR(20) NOT NULL,
+    FOREIGN KEY (`ID`) REFERENCES `Distributor` (`ID`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE `TV`
 (
-  `ID` INTEGER PRIMARY KEY,
-  `screensize` FLOAT,
-  `Model_No` VARCHAR(20),
-  `screentype` VARCHAR(10)
+    `ID` INTEGER PRIMARY KEY NOT NULL,
+    `screensize` FLOAT NOT NULL,
+    `Model_No` VARCHAR(20),
+    `screentype` VARCHAR(10),
+    FOREIGN KEY (`ID`) REFERENCES `Category` (`ID`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE `Chargers`
 (
-  `ID` INTEGER PRIMARY KEY,
-  `voltage` FLOAT
+    `ID` INTEGER PRIMARY KEY NOT NULL,
+    `voltage` FLOAT NOT NULL,
+    `charger_Type` VARCHAR(40) NOT NULL,
+    FOREIGN KEY (`ID`) REFERENCES `Category` (`ID`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE `Category`
 (
-  `ID` INTEGER PRIMARY KEY,
-  `name` VARCHAR(100),
-  `weight` FLOAT,
-  `company` VARCHAR(50),
-  `length` FLOAT,
-  `width` FLOAT,
-  `height` FLOAT,
-  `desc` VARCHAR(500)
+    `ID` INTEGER PRIMARY KEY NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `weight` FLOAT NOT NULL,
+    `company` VARCHAR(50) NOT NULL,
+    `length` FLOAT NOT NULL,
+    `width` FLOAT NOT NULL,
+    `height` FLOAT NOT NULL,
+    `desc` VARCHAR(500)
 );
 
 CREATE TABLE `Mobiles`
 (
-  `ID` INTEGER PRIMARY KEY,
-  `screensize` FLOAT,
-  `Model_No` VARCHAR(20)
+    `ID` INTEGER PRIMARY KEY NOT NULL,
+    `screensize` FLOAT NOT NULL,
+    `Model_No` VARCHAR(20) NOT NULL,
+    FOREIGN KEY (`ID`) REFERENCES `Category` (`ID`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE `Video_Games`
 (
-  `ID` INTEGER PRIMARY KEY,
-  `genre` VARCHAR(20),
-  `release_date` DATE
+    `ID` INTEGER PRIMARY KEY NOT NULL,
+    `genre` VARCHAR(20),
+    `release_date` DATE,
+    FOREIGN KEY (`ID`) REFERENCES `Category` (`ID`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE `platform`
 (
-  `name` VARCHAR(20) PRIMARY KEY,
-  `version` VARCHAR(20) PRIMARY KEY,
-  `video_game_id` INTEGER PRIMARY KEY
+    `name` VARCHAR(20) NOT NULL,
+    `version` VARCHAR(20) NOT NULL,
+    `video_game_id` INTEGER PRIMARY KEY NOT NULL,
+    FOREIGN KEY (`video_game_id`) REFERENCES `Video_Games` (`ID`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE `Color`
 (
-  `color_pallete` VARCHAR(10) PRIMARY KEY,
-  `Category_ID` INTEGER PRIMARY KEY
+    `color_pallete` VARCHAR(10) NOT NULL,
+    `Category_ID` INTEGER PRIMARY KEY NOT NULL,
+    FOREIGN KEY (`Category_ID`) REFERENCES `Category` (`ID`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
 );
-
-ALTER TABLE `Supplier` ADD FOREIGN KEY (`ID`) REFERENCES `Entity` (`ID`);
-
-ALTER TABLE `Items` ADD FOREIGN KEY (`supplier_ID`) REFERENCES `Supplier` (`ID`);
-
-ALTER TABLE `Items` ADD FOREIGN KEY (`distributor_ID`) REFERENCES `Distributor` (`ID`);
-
-ALTER TABLE `Items` ADD FOREIGN KEY (`Category_ID`) REFERENCES `Category` (`ID`);
-
-ALTER TABLE `Distributor` ADD FOREIGN KEY (`ID`) REFERENCES `Entity` (`ID`);
-
-ALTER TABLE `Company` ADD FOREIGN KEY (`ID`) REFERENCES `Distributor` (`ID`);
-
-ALTER TABLE `Person` ADD FOREIGN KEY (`ID`) REFERENCES `Distributor` (`ID`);
-
-ALTER TABLE `TV` ADD FOREIGN KEY (`ID`) REFERENCES `Category` (`ID`);
-
-ALTER TABLE `Chargers` ADD FOREIGN KEY (`ID`) REFERENCES `Category` (`ID`);
-
-ALTER TABLE `Mobiles` ADD FOREIGN KEY (`ID`) REFERENCES `Category` (`ID`);
-
-ALTER TABLE `Video_Games` ADD FOREIGN KEY (`ID`) REFERENCES `Category` (`ID`);
-
-ALTER TABLE `platform` ADD FOREIGN KEY (`video_game_id`) REFERENCES `Video_Games` (`ID`);
-
-ALTER TABLE `Color` ADD FOREIGN KEY (`Category_ID`) REFERENCES `Category` (`ID`);
